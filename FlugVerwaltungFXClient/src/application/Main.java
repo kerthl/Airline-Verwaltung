f236d8038bfa2fa852aa36979e51bdf8f77c1af0
pupil <pupil@localhost>
2019-10-30 09:06:00 +0100

    another comment

--- FlugVerwaltungFXClient/src/application/Main.java
+++ FlugVerwaltungFXClient/src/application/Main.java
@@ -5,7 +5,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
-// ein schöner Kommentar
+// ein schöner Kommentar immer noch
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
