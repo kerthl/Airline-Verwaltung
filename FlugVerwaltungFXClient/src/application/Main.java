ab45ff6ab28ed7caa9c8325897659550ba9b2812
pupil <pupil@localhost>
2019-10-30 09:07:32 +0100

    asdf

--- FlugVerwaltungFXClient/src/application/Main.java
+++ FlugVerwaltungFXClient/src/application/Main.java
@@ -5,7 +5,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
-// ein schöner Kommentar immer noch
+// ein schöner Kommentar immer noch asdf
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
