be2718c01c1060a9aa2edd0e11a453e24034e186
Sebastian Mauch <mauchs@edu.htl-villach.at>
2019-12-04 12:07:22 +0100

    Added Menu

--- AngularProjekt/src/app/app.component.ts
+++ AngularProjekt/src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
+  // tslint:disable: variable-name
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularFluegeKarte';
-  
+  @HostListener('window:resize', ['$event'])
+
+  title = 'Angular Fluege Karte';
+  private _opened = false;
+  private _autoCollapseHeight: number = window.innerHeight * 0.7;
+  private _autoCollapseWidth: number = window.innerWidth * 0.7;
+
+  private _toggleOpened(): void {
+    this._opened = !this._opened;
+  }
+
 }
--- AngularProjekt/src/app/app.module.ts
+++ AngularProjekt/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
+import { SidebarModule } from 'ng-sidebar';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SidebarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
