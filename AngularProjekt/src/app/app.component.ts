77b2d918a7baf8bb14b6df9c54314a349bdfa65a
Sebastian Mauch <mauchs@edu.htl-villach.at>
2019-12-09 09:10:31 +0100

    Added Map

--- AngularProjekt/src/app/app.component.ts
+++ AngularProjekt/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, HostListener } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
-  // tslint:disable: variable-name
+// tslint:disable: variable-name
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +9,10 @@ import { Component, HostListener } from '@angular/core';
 })
 export class AppComponent {
   @HostListener('window:resize', ['$event'])
+  map: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/streets-v11';
+  lat = 47.61028;
+  lng = 13.85583;
 
   title = 'Angular Fluege Karte';
   private _opened = false;
@@ -16,6 +21,18 @@ export class AppComponent {
 
   private _toggleOpened(): void {
     this._opened = !this._opened;
+  }
+
+  constructor() { }
+  ngOnInit() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibWF1Y2hzIiwiYSI6ImNrM3kzaGMxOTBpM3UzZHFjdXN1ZjRsM3UifQ.ZpBwuft2KhU2B_eBKiIkew';
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      zoom: 7,
+      center: [this.lng, this.lat]
+    });    // Add map controls
+    this.map.addControl(new mapboxgl.NavigationControl());
   }
 
 }
